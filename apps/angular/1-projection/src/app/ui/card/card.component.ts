e6998ef60d9e1d34119b1bb4037c055b13516cf2
naithagoni <naithagoni91@gmail.com>
2025-05-21 13:22:22 +0200

    Refactored CardComponent

--- apps/angular/1-projection/src/app/ui/card/card.component.ts
+++ apps/angular/1-projection/src/app/ui/card/card.component.ts
@@ -6,6 +6,11 @@ import {
   output,
   TemplateRef,
 } from '@angular/core';
+import { City } from '../../model/city.model';
+import { Student } from '../../model/student.model';
+import { Teacher } from '../../model/teacher.model';
+
+type Items = Student | Teacher | City;
 
 @Component({
   selector: 'app-card',
@@ -31,9 +36,8 @@ import {
     class: 'border-2 border-black rounded-md p-4 w-fit flex flex-col gap-3',
   },
 })
-export class CardComponent {
-  readonly items = input<any[] | null>(null);
-  readonly customClass = input('');
+export class CardComponent<T extends Items> {
+  readonly items = input<T[] | null>(null);
   readonly add = output();
   // Capture projected ng-template
   readonly listTpl = contentChild.required(TemplateRef);
