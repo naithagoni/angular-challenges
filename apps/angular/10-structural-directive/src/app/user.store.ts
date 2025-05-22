77ba090a21d0241925ffe37322e9a189a2402c76
naithagoni <naithagoni91@gmail.com>
2025-05-22 17:01:44 +0200

    Organized folders

---
+++ apps/angular/10-structural-directive/src/app/user.store.ts
@@ -0,0 +1,14 @@
+import { Injectable, signal } from '@angular/core';
+import { User } from './user.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserStore {
+  private _user = signal<User | undefined>(undefined);
+  public user = this._user.asReadonly;
+
+  add(user: User) {
+    this._user.set(user);
+  }
+}
---
+++ apps/angular/13-change-detection-bug/README.md
@@ -0,0 +1,13 @@
+# Change Detection Bug
+
+> author: thomas-laforge
+
+### Run Application
+
+```bash
+npx nx serve angular-change-detection-bug
+```
+
+### Documentation and Instruction
+
+Challenge documentation is [here](https://angular-challenges.vercel.app/challenges/angular/32-change-detection-bug/).
